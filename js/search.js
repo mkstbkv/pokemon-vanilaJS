d68fecb04c1617a82b8fd6a1c9898802731f0ad9
Tugolbai Maksatbek uulu <mkstbkv751@gmail.com>
2022-11-28 02:45:01 +0600

    3) Сортировка по имени, росту, весу, типу

--- js/search.js
+++ js/search.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon/';
     let data = null;
     let newData = [];
-    const url = 'https://pokeapi.co/api/v2/pokemon/';
     let limit = 10;
     const resultsBlock = document.getElementById('result-block');
     const paginationBlock = document.getElementById('pagination');
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const inputText = document.getElementById('inputText');
     const inputNumber = document.getElementById('inputNumber');
     const sortSelect = document.getElementById('sortSelect');
+    const orderSelect = document.getElementById('orderSelect');
 
     const fetchData = async url => {
         const response = await fetch(url);
@@ -18,14 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log('Что то пошло не так. Ошибка: ' + response.status);
         }
     };
-
-    function byField (field, order) {
-        if (order === 'increase') {
-            return (a, b) => a[field] > b[field] ? 1 : -1;
-        } else if (order === 'decrease') {
-            return (a, b) => b[field] > a[field] ? 1 : -1;
-        }
-    }
 
     function numberOfButtons (arr, num) {
         return Math.ceil(arr.length/num )
@@ -41,12 +34,15 @@ document.addEventListener('DOMContentLoaded', () => {
     async function paintResult (arr) {
         for ( i = 0, r = ""; i < arr.length; i++ ) {
             const getPokemonInfo = await fetchData(url + arr[i].name);
+            const types = getPokemonInfo.types.map(t => t.type.name);
+
             r += `
                 <div class="result-card">
-                    <img src="${ getPokemonInfo['sprites']['front_default'] }" alt="${ getPokemonInfo.name }">
+                    <img src="${ getPokemonInfo['sprites']['front_default'] }" alt="${ getPokemonInfo.name }"/>
                     <p>${ getPokemonInfo.name }</p>
                     <p>Рост: ${ getPokemonInfo.height }</p>
                     <p>Вес: ${ getPokemonInfo.weight }</p>
+                    <p>Тип: ${ types.join(', ') }</p>
                     <button>Добавить в избранное</button>
                     <a href="../pages/pokemon-info.html" id="pokemon-info">More info</a>  
                 </div>
@@ -63,21 +59,58 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const searchPagination = async arr => {
         resultsBlock.innerHTML = await paintResult(arr.slice(0, limit));
-        paginationBlock.innerHTML = paintPaginationButton(numberOfButtons(data['results'], limit));
+        paginationBlock.innerHTML = paintPaginationButton(numberOfButtons(data.results, limit));
         paginationBlock.children[0].classList.add('active');
     }
 
-    const searchInArr = ()  => {
-        data['results'].forEach( pokemon => {
-            if (pokemon['name'].includes(inputText.value)) {
-                newData.push(pokemon);
+    function sortInArr (field, order) {
+        if (order === 'increase') {
+            if (field === 'type') {
+                return (a, b) => a.types[0].type.name > b.types[0].type.name ? 1 : -1;
+            } else {
+                return (a, b) => a[field] > b[field] ? 1 : -1;
             }
-        });
+        } else if (order === 'decrease') {
+            if (field === 'type') {
+                return (a, b) => a.types[0].type.name > b.types[0].type.name ? -1 : 1;
+            } else {
+                return (a, b) => a[field] > b[field] ? -1 : 1;
+            }
+        }
+        return 0;
     }
+
+    const submit = async (input, sort, order,) => {
+        let pokemonData = [];
+
+        if (!(input === '')) {
+            data.results.forEach( pokemon => {
+                if (pokemon.name.includes(input)) {
+                    newData.push(pokemon);
+                }
+            });
+
+            for (let i = 0; i < newData.length; i++) {
+                let pokemon = await fetchData(url + newData[i].name)
+                pokemonData.push(pokemon);
+            }
+        } else {
+            for (let i = 0; i < data.results.length; i++) {
+                let pokemon = await fetchData(url + data.results[i].name)
+                pokemonData.push(pokemon);
+            }
+        }
+
+        data = {...data, results: pokemonData};
+        data.results.sort(sortInArr(sort, order));
+
+        paginationBlock.innerHTML = '';
+    }
+
 
     const run = async () => {
         data = await fetchData(url + '?limit=100000&offset=0');
-        await searchPagination(data['results']);
+        await searchPagination(data.results);
 
         document.addEventListener('click', async function (event){
             if ([...event.target.classList].includes("pagination-button")) {
@@ -85,45 +118,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 const y = event.target.textContent;
                 const start = limit * (y - 1);
                 const end = limit * y;
-                resultsBlock.innerHTML = await paintResult(data['results'].slice(start, end));
-            } else {
-                // console.log('');
+                resultsBlock.innerHTML = await paintResult(data.results.slice(start, end));
             }
         });
 
         form.addEventListener('submit', async e => {
             e.preventDefault();
             data = await fetchData(url + '?limit=100000&offset=0');
-            if (!data || (data['results'].length === 0)) {
+            if (!data || (data.results.length === 0)) {
                 return;
             }
             newData = [];
             limit = inputNumber.value;
-
-            if ((inputText.value) && (sortSelect.value === 'a-z')) {
-                searchInArr();
-                data = {...data, ['results']: newData.sort(byField('name', 'increase'))};
-                paginationBlock.innerHTML = '';
-            }
-
-            if ((inputText.value) && (sortSelect.value === 'z-a')) {
-                searchInArr();
-                data = {...data, ['results']: newData.sort(byField('name', 'decrease'))};
-                paginationBlock.innerHTML = '';
-            }
-
-            if ((inputText.value === '') && (sortSelect.value === 'a-z')) {
-                data['results'].sort(byField('name', 'increase'))
-                paginationBlock.innerHTML = '';
-            }
-
-            if ((inputText.value === '') && (sortSelect.value === 'z-a')) {
-                data['results'].sort(byField('name', 'decrease'))
-                paginationBlock.innerHTML = '';
-            }
-
-            await searchPagination(data['results'])
+            await submit(inputText.value, sortSelect.value, orderSelect.value);
+            await searchPagination(data.results);
         });
     };
+
     run();
 });
